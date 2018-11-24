48ecd7e0e40ee50928be4198299e0999cd2e5d0d
Juan Pablo Eslava <jpesmar@gmail.com>
2018-11-24 16:50:54 +0100

    JP - correction: correction to previous commit

--- src/1-views/Home.js
+++ src/1-views/Home.js
@@ -28,6 +28,27 @@ class Home extends React.Component {
                 console.log(error);
             })
     }
+
+    // Manage books Method:
+    // move books between shelves (and from search page to shelves)
+    // passed down as prop to Shelf, and from Shelf as prop to Book
+    moveBook = (book, targetShelf) => {
+        console.log(`The book '${book.title}' will be moved to the '${targetShelf}' shelf.`);
+        // call method from API
+        BooksAPI.update(book, targetShelf)
+            .then((resp) => {
+                // console.log(resp); // the response is given with book keys
+                // pass desired shelf as the new .shelf prop for the book
+                book.shelf = targetShelf;
+                this.setState(state => ({
+                    books: state.books
+                }));
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    
     render() {
 
         return (
@@ -38,26 +59,33 @@ class Home extends React.Component {
                 <div className="list-books-content">
                     <div>
                         <Shelf 
+                            // pass 2 props for title and books
                             shelfTitle="Currently Reading"
                             booksInShelf={
                                 this.state.books.filter( book => 
                                     book.shelf === 'currentlyReading'    
                                 )
-                            } />
+                            } 
+                            moveBook={this.moveBook}
+                        />
                         <Shelf 
                             shelfTitle="Want to Read" 
                             booksInShelf={
                                 this.state.books.filter( book => 
                                     book.shelf === 'wantToRead'    
                                 )
-                            } />
+                            }
+                            moveBook={this.moveBook}
+                        />
                         <Shelf 
                             shelfTitle="Read" 
                             booksInShelf={
                                 this.state.books.filter( book => 
                                     book.shelf === 'read'    
                                 )
-                            } />
+                            }
+                            moveBook={this.moveBook}
+                        />
                     </div>
                 </div>
                 <OpenSearch />
--- src/2-components/Shelf.js
+++ src/2-components/Shelf.js
@@ -9,14 +9,23 @@ class Shelf extends React.Component {
     render() {
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+                <h2 className="bookshelf-title">
+                    {/* use title prop passed during invocation in Home */}
+                    {this.props.shelfTitle}
+                </h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         {
+                            // use props passed during invocation in Home
                             this.props.booksInShelf.map ( book => (
                                 <Book 
+                                    // pass 3 props for:
+                                    // - key(otherwise it throws an error)
+                                    // - individual book (it'll then have all books data as an object)
+                                    // - moveBook Method (originally in Home and Search)
                                     key={book.id} 
                                     thisBook={book}
+                                    moveBook={this.props.moveBook}
                                 />
                             ))
                         }
